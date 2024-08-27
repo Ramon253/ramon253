a1634060c46c8444a81c4ce51f8b6adbe6a9502c
Ramon253 <jgalazc253@g.educaand.es>
2024-08-27 23:58:34 +0200

    First components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {IndexComponent} from "./components/index/index.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+  }
+];
