e5a6cb3166b8e42b17e0ab9724762860eebc1126
Ramon253 <jgalazc253@g.educaand.es>
2024-08-29 22:28:43 +0200

    First components

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {NavItem} from '../../interfaces/nav-item';
 @Component({
   selector: 'app-layout',
   standalone: true,
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-
+  navItems : Array<NavItem> = [
+    {
+      name : 'Proyectos',
+      link: 'Projects'
+    },
+    {
+      name : 'Sobre mi',
+      link : 'about',
+    },
+    {
+      name: 'Contacto',
+      link: 'contac',
+    }
+  ]
 }
