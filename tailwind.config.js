67004958da9c0faf7d72df65113159ab9c5529b7
Ramon253 <jgalazc253@g.educaand.es>
2024-08-29 21:08:06 +0200

    First components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        mainGrey : '#2F2F2F',
+        main : '#222321',
+        secondary : '#709E64',
+        mainWhite: '#CBCBCB',
+        secondaryWhite: '#CBCBCB',
+      },
+      fontFamily: {
+        'nunito' : ['Nunito'],
+      },
+      boxShadow : {
+        'main-shadow' : 'rgba(240,46,170,0.4) 5px 5px, rgba(240,46,170,0.3) 10px 10px, rgba(240,46,170,0.2) 15px 15px, rgba(240,46,170,0.1) 20px 20px, rgba(240,46,170,0.05) 25px 25px',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    addDynamicIconSelectors()
+  ],
 }
 
